9bea458ea0863fa566c0818eb5fb7b41f3d4400e
Nandi <nandishbhushan27@gmail.com>
2022-12-21 21:31:11 +0530

    added new files to branch

---
+++ second.py
@@ -0,0 +1,24 @@
+# second file for today
+
+def main():
+    sum(4,2)
+    sub(4,2)
+    multi(4,2)
+    div(4,2)
+    print(sum(4,2))
+
+def sum(n,m):
+    return n+m
+
+def sub(n,m):
+    return n-m
+
+def multi(n,m):
+    return n*m
+
+def div(n,m):
+    return n/m
+    
+if __name__ == "__main__":
+    main()
+    
