222bdf79c8627830d501fff44c73061cd0f50863
nandish <nandishbhushan27@gmail.com>
2022-12-22 15:01:38 +0530

    push from laptop

--- dec22/python/numbers.py
+++ dec22/python/numbers.py
@@ -1,1 +1,6 @@
 # accepts a sequence of comma-separated numbers from user and generate a list and a tuple with those numbers.
+
+
+numbers = input("enter numbers")
+numbers.split(",")
+print(numbers)
